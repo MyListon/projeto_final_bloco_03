97a6553744fdd60caf22273924c3f30cc91d271c
MyListon <myliston@gmail.com>
2025-12-02 10:00:54 -0300

    :sparkles:Implementação Home, Navbar e Footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-green-900 text-white'>
+            
+                <div className="container flex justify-between text-lg mx-8">
+                    Vita Care
+
+                    <div className='flex gap-4'>
+                        Home
+                        Perfil
+                        Sair
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
